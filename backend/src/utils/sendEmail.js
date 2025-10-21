bb9a097aace65a38e1185e2274bc589aa19938b5
dhruv <dhruvpatel152002@gmail.com>
2025-10-21 00:35:44 -0400

    Implmeneted the appointment reminder functionality

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,13 +4,13 @@ const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "ENTER_SENDER_EMAIL_HERE", 
-      pass: "ENTER_SENDER_EMAIL_PASSWORD_HERE",
-    },
+      user: "practice0003@gmail.com", 
+      pass: "zrdtrjdmgtrtljkr"
+    },    
   });
 
   await transporter.sendMail({
-    from: `"DocMedaa" <ENTER_SENDER_EMAIL_HERE>`,
+    from: `"DocMedaa" <practice0003@gmail.com>`,
     to,
     subject,
     html,
