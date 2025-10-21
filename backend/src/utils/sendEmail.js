d0cec36a5499cad0fc5c98e281590df93bf51da4
dhruv <dhruvpatel152002@gmail.com>
2025-10-20 23:23:25 -0400

    Updated the forgot password functionality

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,13 +4,13 @@ const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "practice0003@gmail.com", 
-      pass: "ifsnlxmuisysykph",
+      user: "ENTER_SENDER_EMAIL_HERE", 
+      pass: "ENTER_SENDER_EMAIL_PASSWORD_HERE",
     },
   });
 
   await transporter.sendMail({
-    from: `"DocMedaa" <practice0003@gmail.com>`,
+    from: `"DocMedaa" <ENTER_SENDER_EMAIL_HERE>`,
     to,
     subject,
     html,
