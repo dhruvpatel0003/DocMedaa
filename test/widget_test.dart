b6a80623f86dde11f12be7a2c73276944199766a
Prutha Trivedi <prutha19121@gmail.com>
2025-10-28 19:08:52 -0400

    Added doctor appointment screen, patient detail screen, and updated tests and CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:docmedaa_frontend/main.dart'; // <-- make sure this matches your pubspec name
+import 'package:docmedaa_frontend/main.dart'; 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
